9249c75d8b02756350bd62d4fc7ea522fd7fe403
Julia <juliatlewand@gmail.com>
2023-11-11 11:27:02 -0500

    Initial commit

---
+++ server.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const axios = require('axios');
+const bodyParser = require('body-parser');
+require('dotenv').config();
+
+const app = express();
+app.use(bodyParser.json());
+app.use(express.static('.'));
+
+const OPENAI_API_URL = 'https://api.openai.com/v1/engines/davinci/completions';
+const OPENAI_API_KEY = '';
+
+app.post('/getResponse', async (req, res) => {
+    const userMessage = req.body.message;
+    try {
+        const response = await axios.post(OPENAI_API_URL, {
+            prompt: userMessage,
+            max_tokens: 150
+        }, {
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${OPENAI_API_KEY}`
+            }
+        });
+
+        res.json({ reply: response.data.choices[0].text.trim() });
+    } catch (error) {
+        if (error.response) {
+            // The request was made and the server responded with a status code
+            // that falls out of the range of 2xx
+            console.error("Error data: ", error.response.data);
+            console.error("Error status: ", error.response.status);
+            console.error("Error headers: ", error.response.headers);
+        } else if (error.request) {
+            // The request was made but no response was received
+            console.error("Error request: ", error.request);
+        } else {
+            // Something happened in setting up the request that triggered an Error
+            console.error('Error', error.message);
+        }
+        res.json({ reply: "Error: Unable to fetch response from OpenAI." });
+    }
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
